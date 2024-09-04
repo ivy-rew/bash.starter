6c562ab8170840d9743864184697c1b6e437e02f
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-09-04 17:37:53 +0200

    voice

--- hello.sh
+++ hello.sh
@@ -3,7 +3,8 @@
 sayHello(){
   echo "Who are you?"
   read user
-  echo "hello ${user}"
+  welcome="Hallo du, wie gehts dir ${user} ?"
+  espeak "$welcome"
   if [[ "$user" == "Fritz" ]]; then
     echo "Glad you came over!"
   fi
